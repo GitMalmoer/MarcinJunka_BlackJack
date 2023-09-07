87f1731020ac472f9aca1f94042f0fe3c863b53a
Marcin2 <marcinjunka@gmail.com>
2023-09-07 23:40:43 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Business_Logic;
+
+namespace MarcinJunka_BlackJack
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShuffle_Click(object sender, RoutedEventArgs e)
+        {
+            GameDeck gd = new GameDeck();
+            gd.GenerateDeck();
+        }
+    }
+}
