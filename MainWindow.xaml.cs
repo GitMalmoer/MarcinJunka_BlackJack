d7f315dd631476b9c8dab62a05c548b68f42370d
Marcin2 <marcinjunka@gmail.com>
2023-09-15 22:56:18 +0200

    BlackJackFinalV1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -36,13 +36,9 @@ namespace MarcinJunka_BlackJack
             txtSaldo.Text = GameMechanics.GetPlayerSaldo().ToString();
         }
 
-        private void btnShuffle_Click(object sender, RoutedEventArgs e)
+        private void btnNewGame_Click(object sender, RoutedEventArgs e)
         {
-            //GameDeck gd = new GameDeck();
-            //gd.GenerateDeck();
-
-            //MessageBox.Show(gd.GetRandomCard().ToString());
-
+            NewGame();
         }
 
         private void BoolMessage(bool check, string? trueMessage, string? falseMessage)
@@ -88,6 +84,12 @@ namespace MarcinJunka_BlackJack
             GameMechanics.BetIsPlaced += GameMechanics_BetIsPlacedEvent;
             GameMechanics.CardIsDrawn += GameMechanics_CardIsDrawnEvent;
 
+            if (GameMechanics.GetGameStatus())
+            {
+                MessageBox.Show("You can deal only once after placing a bet.");
+                return;
+            }
+
             bool isDealSucessed = GameMechanics.Deal();
             if (!isDealSucessed)
             {
@@ -102,7 +104,7 @@ namespace MarcinJunka_BlackJack
             GameMechanics.BetIsPlaced -= GameMechanics_BetIsPlacedEvent;
         }
 
-        private void AssignDealerImages()
+        private void AssignAllDealerImages()
         {
             List<GameCard> dealerCards = GameMechanics.GetDealerHandCards();
             List<Image> dealerImages = new List<Image>()
@@ -112,12 +114,37 @@ namespace MarcinJunka_BlackJack
                 imgDealerCard3,
                 imgDealerCard4,
                 imgDealerCard5,
+                imgDealerCard6,
+                imgDealerCard7,
             };
 
             for (int i = 0; i < dealerCards.Count; i++)
             {
                 dealerImages[i].Source = InitializeBitmapImage(dealerCards[i].ImgPath);
             }
+        }
+        /// <summary>
+        /// This method assigns card images to dealer only at beginning when player has not clicked 'stand' button yet.
+        /// </summary>
+        private void AssignDealerImages()
+        {
+            List<GameCard> dealerCards = GameMechanics.GetDealerHandCards();
+            List<Image> dealerImages = new List<Image>()
+            {
+                imgDealerCard1,
+                imgDealerCard2,
+            };
+
+            dealerImages[0].Source = InitializeBitmapImage(GameDeck.UnknownCardPath);
+            dealerImages[1].Source = InitializeBitmapImage(dealerCards[1].ImgPath);
+
+            var card = GameMechanics.GetDealerHandCards()[1];
+        }
+
+        private int GetDealerScoreWhenCardIsHidden()
+        {
+            GameCard secondCard = GameMechanics.GetDealerHandCards()[1];
+            return secondCard.CardRank.Value;
         }
 
         private void AssignPlayerImages()
@@ -130,6 +157,8 @@ namespace MarcinJunka_BlackJack
                 imgPlayerCard3,
                 imgPlayerCard4,
                 imgPlayerCard5,
+                imgPlayerCard6,
+                imgPlayerCard7,
             };
 
             for (int i = 0; i < playerCards.Count ; i++)
@@ -149,29 +178,100 @@ namespace MarcinJunka_BlackJack
 
         private void btnHit_Click(object sender, RoutedEventArgs e)
         {
+            if (!GameMechanics.GetGameStatus())
+            {
+                MessageBox.Show("You need to deal first");
+                return;
+            }
+
             GameMechanics.CardIsDrawn += GameMechanics_CardIsDrawnEvent;
+            GameMechanics.CheckForWinnerEventHandler += GameMechanics_CheckForWinnerEvent;
             GameMechanics.Hit();
+            // unsubscribe to event because otherwise it will check for winner few times in a row.
+            GameMechanics.CheckForWinnerEventHandler -= GameMechanics_CheckForWinnerEvent;
         }
 
         private void GameMechanics_CardIsDrawnEvent(object? sender, EventArgs e)
         {
-            AssignPlayerImages();
-            AssignDealerImages();
-            txtScoreDealer.Text = GameMechanics.GetDealerScore().ToString();
-            txtScorePlayer.Text = GameMechanics.GetPlayerScore().ToString();
+            if (!GameMechanics.CheckPlayerStanding())
+            {
+                AssignPlayerImages();
+                AssignDealerImages();
+                txtScorePlayer.Text = GameMechanics.GetPlayerScore().ToString();
+                txtScoreDealer.Text = GetDealerScoreWhenCardIsHidden().ToString();
+            }
+            else
+            {
+                AssignPlayerImages();
+                AssignAllDealerImages();
+                txtScoreDealer.Text = GameMechanics.GetDealerScore().ToString();
+                txtScorePlayer.Text = GameMechanics.GetPlayerScore().ToString();
+            }
+
             GameMechanics.CardIsDrawn -= GameMechanics_CardIsDrawnEvent;
         }
 
         private void btnStand_Click(object sender, RoutedEventArgs e)
         {
+            if (!GameMechanics.GetGameStatus())
+            {
+                MessageBox.Show("You need to deal first");
+                return;
+            }
+
             GameMechanics.CardIsDrawn += GameMechanics_CardIsDrawnEvent;
             GameMechanics.CheckForWinnerEventHandler += GameMechanics_CheckForWinnerEvent;
             GameMechanics.Stand();
         }
 
-        private void GameMechanics_CheckForWinnerEvent(object? sender, EventArgs e)
+        private void GameMechanics_CheckForWinnerEvent(object? sender, CheckForWinEventArgs e)
         {
-            throw new NotImplementedException();
+            MessageBox.Show(e.winnerMessage);
+            txtSaldo.Text = e.currentSaldo.ToString();
+            NextRound();
+            GameMechanics.CheckForWinnerEventHandler -= GameMechanics_CheckForWinnerEvent;
         }
+
+
+        private void NewGame()
+        {
+            GameMechanics.NewGame();
+            ClearImages();
+            ClearScore();
+            // CLEAR SALDO
+            txtSaldo.Text = GameMechanics.GetPlayerSaldo().ToString();
+            txtBet.Text = GameMechanics.CurrentBet.ToString();
+        }
+
+        private void NextRound()
+        {
+            ClearImages();
+            ClearScore();
+            txtBet.Text = GameMechanics.CurrentBet.ToString();
+        }
+        private void ClearImages()
+        {
+
+            StackPanel stackPanelDealer = dealerImagesPanel;
+            StackPanel stackPanelPlayer = playerImagesPanel;
+
+            foreach (Image image in stackPanelDealer.Children)
+            {
+                image.Source = null;
+            }
+
+            foreach (Image image in stackPanelPlayer.Children)
+            {
+                image.Source = null;
+            }
+        }
+
+        private void ClearScore()
+        {
+            txtScoreDealer.Text = "0";
+            txtScorePlayer.Text = "0";
+        }
+
+
     }
 }
